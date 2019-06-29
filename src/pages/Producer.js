6888d512749997339a9e06fd6a29452ee809898c
YANG XU <yaxu2@student.unimelb.edu.au>
2019-06-29 18:09:09 +1000

    [init] shop

--- src/pages/Producer.js
+++ src/pages/Producer.js
@@ -8,13 +8,40 @@ export class Producer extends Component {
     render() {
         return (
             <div className='producer'>
-                <section className='background'>
+                <div className='background'>
                     <ParticlesWrapper />
-                </section>
+                </div>
                 <MediaPlayer />
-                <section className='intro'>
-                    <h1>Welcome EggTronic's Music Center</h1>
-                </section>
+                <div className='intro'>
+                    <div className='grid-1'>
+
+                    </div>
+                    <div className='grid-2'>
+
+                    </div>
+                    <div className='grid-3'>
+
+                    </div>
+                    <div className='grid-4'>
+
+                    </div>
+                    <div className='grid-5'>
+                        <h1>Welcome EggTronic's Music Center</h1>
+                    </div>
+                    <div className='grid-6'>
+
+                    </div>
+                    <div className='grid-7'>
+
+                    </div>
+                    <div className='grid-8'>
+
+                    </div>
+                    <div className='grid-9'>
+
+                    </div>
+
+                </div>
             </div>
         )
     }
