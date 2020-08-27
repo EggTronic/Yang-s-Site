7479a4f50f4adb2b1e3f387566810ff6c698a1a2
YANG XU <yaxu2@student.unimelb.edu.au>
2020-08-27 21:20:34 +1000

    [add] mikutap iframe demo

--- src/pages/MusicPage/MusicPage.js
+++ src/pages/MusicPage/MusicPage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import Cloud from '../../components/Cloud/Cloud';
 
 const MusicPageWrapper = styled.div`
   position: absolute;
   box-sizing: border-box;
-  min-height: calc(100vh - 164px);
+  height: calc(100vh - 164px);
   width: 90vw;
   top: 100px;
   background: rgba(111,111,111,0.4);
@@ -14,8 +13,14 @@ const MusicPageWrapper = styled.div`
 function MusicPage() {
   return (
     <MusicPageWrapper>
-      <Cloud />
-      Music;
+      This is made by AIDN. I really love this idea and I am trying to recreate it! So Cool!
+      <iframe
+        src="https://aidn.jp/mikutap/"
+        title="MikuTap"
+        height="100%"
+        width="100%"
+      >
+      </iframe>
     </MusicPageWrapper>
   );
 }
