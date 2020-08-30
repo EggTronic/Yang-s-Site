2351b6ae88117128396092adc3d774761be512de
YANG XU <yaxu2@student.unimelb.edu.au>
2020-08-30 13:01:34 +1000

    [update] audio samples

--- src/assets/js/audios/index.js
+++ src/assets/js/audios/index.js
@@ -1,21 +1,22 @@
-import k1 from './sample - b4.wav';
-import k2 from './sample - b5.wav';
-import k3 from './sample - b6.wav';
-import k4 from './sample - c5.wav';
-import k5 from './sample - c6.wav';
-import k6 from './sample - c7.wav';
-import k7 from './sample - d5.wav';
-import k8 from './sample - d6.wav';
-import k9 from './sample - d7.wav';
-import k10 from './sample - e5.wav';
-import k11 from './sample - e6.wav';
-import k12 from './sample - e7.wav';
-import k13 from './sample - f5.wav';
-import k14 from './sample - f6.wav';
-import k15 from './sample - f7.wav';
-import k16 from './sample - g5.wav';
-import k17 from './sample - g6.wav';
-import k18 from './sample - g7.wav';
+import k1 from './v1.wav';
+import k2 from './v2.wav';
+import k3 from './v3.wav';
+import k4 from './v4.wav';
+import k5 from './v5.wav';
+import k6 from './v6.wav';
+import k7 from './v7.wav';
+import k8 from './v8.wav';
+import k9 from './v9.wav';
+import k10 from './v10.wav';
+import k11 from './v11.wav';
+import k12 from './v12.wav';
+import k13 from './v13.wav';
+import k14 from './v14.wav';
+import k15 from './v15.wav';
+import k16 from './v16.wav';
+import k17 from './v17.wav';
+import k18 from './v18.wav';
+import k19 from './v19.wav';
 
 export const audioSlices = {
   'k1': k1,
@@ -36,18 +37,18 @@ export const audioSlices = {
   'k16': k16,
   'k17': k17,
   'k18': k18,
-  'k19': k1,
-  'k20': k2,
-  'k21': k3,
-  'k22': k4,
-  'k23': k5,
-  'k24': k6,
-  'k25': k7,
-  'k26': k8,
-  'k27': k9,
-  'k28': k10,
-  'k29': k11,
-  'k30': k12,
-  'k31': k13,
-  'k32': k14
+  'k19': k19,
+  'k20': k13,
+  'k21': k11,
+  'k22': k12,
+  'k23': k1,
+  'k24': k2,
+  'k25': k3,
+  'k26': k4,
+  'k27': k5,
+  'k28': k6,
+  'k29': k7,
+  'k30': k8,
+  'k31': k9,
+  'k32': k10
 };
