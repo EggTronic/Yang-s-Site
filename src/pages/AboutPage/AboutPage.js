f2bcde827ee4906bf334ae5195b21f1747e80227
YANG XU <yaxu2@student.unimelb.edu.au>
2020-10-20 02:30:41 +1100

    [add] about me profile

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -5,6 +5,7 @@ import GlowIcon from '../../components/GlowIcon/GlowIcon';
 import unimelb from '../../assets/images/unimelb.png';
 import uniliverpool from '../../assets/images/uniliverpool.png';
 import monstercat from '../../assets/images/monstercat.png';
+import avatar from '../../assets/images/avatar.png';
 
 const moveGradient = keyframes`
   50% {
@@ -20,15 +21,41 @@ const FrontWrapper = styled.div`
   height: 100%;
 `;
 
+const BackWrapper = styled.div`
+  position: relative;
+  height: 100%;
+`;
+
+const SectionWrapper = styled.div`
+  position: relative;
+  height: calc(100% - 80px);
+  overflow: auto;
+  ::-webkit-scrollbar {
+    width: 5px;
+    position: absolute;
+  }
+  ::-webkit-scrollbar-track {
+    background: #373A40;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: white;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
+
 const SectionHeader = styled.div`
   background: #373A40;
+  width: 100%;
+  height: 80px;
   li {
     margin-bottom: 0px;
   }
 `;
 
 const Img = styled.img`
-  width: 70%;
+  width: ${props => props.width};
   margin-bottom: 10px;
 `;
 
@@ -36,7 +63,15 @@ const Section = styled.section`
   transition: all 0.5s linear;
   background: ${props => props.background};
   margin: 0px;
-  padding: 10px 0px;
+  padding: 0px 0px 10px 0px;
+  height: ${props => props.height};
+  text-align: ${props => props.textAlign};
+  h1, h2, h3, h4, h5, p {
+    margin: 20px;
+  }
+  p, i {
+    line-height: 2;
+  }
 `;
 
 const AboutPageWrapper = styled.div`
@@ -50,8 +85,21 @@ const AboutPageWrapper = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   overflow: auto;
-  &::-webkit-scrollbar {
+  /* ::-webkit-scrollbar {
     display: none;
+  } */
+  ::-webkit-scrollbar {
+    width: 5px;
+    position: absolute;
+  }
+  ::-webkit-scrollbar-track {
+    background: #373A40;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: white;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
   }
   & > div > div {
     display: flex;
@@ -82,56 +130,82 @@ const AboutPageWrapper = styled.div`
 function AboutPage() {
   return (
     <AboutPageWrapper>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-user-secret' />
           <h1>General</h1>
         </FrontWrapper>
-        <div>
+        <BackWrapper>
           <SectionHeader>
             <GlowIcon className='fas fa-user-secret' />
           </SectionHeader>
-          <h1>Male</h1>
-        </div>
+          <SectionWrapper onScroll={e => e.stopPropagation()}>
+            <Section background='#373A40' textAlign='left'>
+              <Img
+                src={avatar}
+                width='100%'
+                alt='avatar'
+              />
+              <h5>Hello</h5>
+              <p>I am Eggtronic, you can recognize me as a web engineer.</p>
+              <p>Age: 25</p>
+              <p>Location: Shanghai</p>
+              <hr />
+              <p>I am a senior programmer with good knowledge of full-stack techiques.
+              I am quite good at learning new techiques and enjoying progress of finding best solutions.
+              I love refactoring my code to ensure it's as DRY as it can be. Also, I am a good team player,
+              I love sharing fancy techniques and ideas. As the saying goes: 'two heads are better than one'.
+            </p>
+            </Section>
+          </SectionWrapper>
+        </BackWrapper>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-graduation-cap' />
           <h1>Education</h1>
         </FrontWrapper>
-        <div>
+        <BackWrapper>
           <SectionHeader>
             <GlowIcon className='fas fa-graduation-cap' />
           </SectionHeader>
-          <Section background='#f38181'>
-            <Img src={unimelb} alt='unimelb' />
-            <br />
-            <p>
-              <i
-                style={{ verticalAlign: 'bottom' }}
-                className="fas fa-calendar-week"
-              /> 2018 - 2020
-            </p>
-            <br />
-            <p>Master Degree</p>
-            <p>Software Engineering</p>
-          </Section>
-          <Section background='#373A40'>
-            <Img src={uniliverpool} alt='uniliverpool' />
-            <br />
-            <p>
-              <i
-                style={{ verticalAlign: 'bottom' }}
-                className="fas fa-calendar-week"
-              /> 2015 - 2017
-            </p>
-            <br />
-            <p>Bachelor Degree</p>
-            <p>Software Development</p>
-          </Section>
-        </div>
+          <SectionWrapper onScroll={e => e.stopPropagation()}>
+            <Section background='#f38181'>
+              <Img
+                src={unimelb}
+                alt='unimelb'
+                width='70%'
+                style={{ paddingTop: '10px' }}
+              />
+              <p>
+                <i
+                  style={{ verticalAlign: 'bottom' }}
+                  className="fas fa-calendar-week"
+                /> 2018 - 2020
+              </p>
+              <p>Master Degree</p>
+              <p>Software Engineering</p>
+            </Section>
+            <Section background='#373A40'>
+              <Img
+                src={uniliverpool}
+                alt='uniliverpool'
+                width='70%'
+                style={{ paddingTop: '10px' }}
+              />
+              <p>
+                <i
+                  style={{ verticalAlign: 'bottom' }}
+                  className="fas fa-calendar-week"
+                /> 2015 - 2017
+              </p>
+              <p>Bachelor Degree</p>
+              <p>Software Development</p>
+            </Section>
+          </SectionWrapper>
+        </BackWrapper>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-bolt' />
           <h1>Skill</h1>
@@ -143,7 +217,7 @@ function AboutPage() {
           <h1>Typescript</h1>
         </div>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-atom' />
           <h1>Experience</h1>
@@ -155,7 +229,7 @@ function AboutPage() {
           <h1>React</h1>
         </div>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-headphones-alt' />
           <h1>Music</h1>
@@ -165,14 +239,14 @@ function AboutPage() {
             <GlowIcon className='fas fa-headphones-alt' />
           </SectionHeader>
           <Section background='white'>
-            <Img src={monstercat} alt='monstercat' />
+            <Img src={monstercat} width='70%' alt='monstercat' />
             <br />
             <h1 style={{ color: 'black' }}>Monster Cat</h1>
             <br />
           </Section>
         </div>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-video' />
           <h1>Film</h1>
@@ -184,7 +258,7 @@ function AboutPage() {
           <h1>Pacific Rim</h1>
         </div>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <i className='nes-charmander' />
           <h1>Anime</h1>
@@ -196,7 +270,7 @@ function AboutPage() {
           <h1>No Game No Life</h1>
         </div>
       </FlipCard>
-      <FlipCard height='100%' width='300px'>
+      <FlipCard height='100%' width='400px'>
         <FrontWrapper>
           <GlowIcon className='fas fa-gamepad' />
           <h1>Game</h1>
